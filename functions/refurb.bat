6adfe6b89ee539fc0a3877efe83813157c839d8a
LucyKurtis <94580910+LucyKurtis@users.noreply.github.com>
2022-10-10 22:33:51 -0800

    BATCH now installs programs and runs one-liner

---
+++ functions/refurb.bat
@@ -0,0 +1,47 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+:main
+echo Connecting to network...
+netsh wlan set hostednetwork mode=allow ssid="Upnorth Computers 5g" key="133SSanta"
+echo.
+echo Connected!
+echo.
+echo Installing Adobe Reader...
+echo.
+echo y | winget install Adobe.Acrobat.Reader.64-bit
+cls
+echo.
+echo Installed Adobe Reader...
+echo.
+echo Installing Java...
+echo y | winget install -e --id Oracle.JavaRuntimeEnvironment
+cls
+echo.
+echo Installed Java...
+echo.
+echo Installing LibreOffice...
+echo y | winget install -e --id TheDocumentFoundation.LibreOffice
+cls
+echo.
+echo Installed LibreOffice...
+echo.
+cls
+echo Executing One-Liner...
+dism /online /cleanup-image /restorehealth
+sfc /scannow
+cls
+
+
+:close
+:: This is the close section, if you want to close your app just type "goto close"
+cls
+exit /b
+:ColorText
+echo off
+echo %DEL% > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
